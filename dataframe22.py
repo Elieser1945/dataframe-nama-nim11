d5846dfa67acb7cd0069cab23efba74f9772c72b
Elieser1945 <elieserpasaribu19@gmail.com>
2024-11-27 19:52:38 +0700

    Create dataframe22.py

---
+++ dataframe22.py
@@ -0,0 +1,13 @@
+import pandas as pd
+
+data = {
+    "Nama": ["Elieser Pasaribu"],
+    "NIM": ["1103223209"]
+}
+
+df = pd.DataFrame(data)
+
+print("Data frame berisi Nama dan NIM")
+print(df)
+
+
